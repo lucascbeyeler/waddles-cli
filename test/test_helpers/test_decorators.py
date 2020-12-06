62056319007a9bbf14e5b43ca82fa6e1e639087b
Lucas Costa Beyeler <lucas.costab@outlook.com>
2020-12-06 22:33:43 +0000

    Fixing pytests for Github Actions

--- test/test_helpers/test_decorators.py
+++ test/test_helpers/test_decorators.py
@@ -1,22 +1,24 @@
+from unittest import mock
+
 import pytest
 
 from waddles_cli.helpers import decorators
 
 
-@pytest.mark.parametrize("content",["database"])
+@pytest.mark.parametrize("content", ["database"])
 def test_requires_config_with_content(content):
-
     @decorators.requires_config
     def test(_):
         return True
 
-    assert test(inner_content=content)
+    with mock.patch("waddles_cli.models.config.ConfigLoader", mock.Mock):
+        assert test(inner_content=content)
 
 
 def test_requires_config_without_content():
-
     @decorators.requires_config
     def test(_):
         return True
 
-    assert test()
+    with mock.patch("waddles_cli.models.config.ConfigLoader", mock.Mock):
+        assert test()
