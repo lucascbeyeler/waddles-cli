892a70a6cd7a41b30c552a138b0eb82835ce6b60
Lucas Costa Beyeler <lucas.costab@outlook.com>
2020-12-07 08:50:00 +0000

    Take 5 - Trying to make Github Actions work

--- test/test_helpers/test_decorators.py
+++ test/test_helpers/test_decorators.py
@@ -6,19 +6,19 @@ from waddles_cli.helpers import decorators
 
 
 @pytest.mark.parametrize("content", ["database"])
+@mock.patch("waddles_cli.models.config.ConfigLoader", mock.Mock)
 def test_requires_config_with_content(content):
     @decorators.requires_config
     def test(_):
         return True
 
-    with mock.patch("waddles_cli.models.config.ConfigLoader", mock.Mock):
-        assert test(inner_content=content)
+    assert test(inner_content=content)
 
 
+@mock.patch("waddles_cli.models.config.ConfigLoader", mock.Mock)
 def test_requires_config_without_content():
     @decorators.requires_config
     def test(_):
         return True
 
-    with mock.patch("waddles_cli.models.config.ConfigLoader", mock.Mock):
-        assert test()
+    assert test()
