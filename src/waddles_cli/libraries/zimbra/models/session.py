77f9b8e63925b622a24ae30b793fa6f47e1f435a
Lucas Costa Beyeler <lucas.costab@outlook.com>
2020-12-19 15:38:48 +0000

    Reorganizing zimbra lib for better reusability

---
+++ src/waddles_cli/libraries/zimbra/models/session.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+from requests import Session
+from requests.auth import HTTPBasicAuth
+
+from waddles_cli.lib.zimbra.constants.urls import ADMIN_URL, START_DATE
+
+class ZimbraAPI:
+
+    def __init__(self, admin_username: str, admin_password: str, webproto: str, server_address: str, admin_port: str):
+        self._session = Session()
+        self._session.verify = False
+        self._session.stream = True
+        self._session.auth = HTTPBasicAuth(admin_username, admin_password)
+        self._url = ADMIN_URL.format(webproto=webproto, server_address=server_address, server_port=admin_port)
+
+
+
+class ZimbraUserAPI(ZimbraAPI):
+
+    def __init__(self, username: str , *args, **kwargs):
+        super(ZimbraAPI).__init__(*args, **kwargs)
+        self.username = username
+
+    def get_account_tgz(self, date:datetime = datetime.now()):
+        """ Download a single package from the Zimbra server based on the informed requirements.
+
+        :param username: The user login you want to retrieve the e-mails
+        :param zimbra_config: A waddles_cli.models.config.Zimbra object with the admin credentials
+        :param date: Not required - the start point to retrieve the backup
+        :return: A requests.session.get output
+        """
+        start_date = START_DATE.format(seconds=date.second)
+        return self._session.get(url=self._url.format(username=self.username, start_date=start_date))
--- src/waddles_cli/operations/download.py
+++ src/waddles_cli/operations/download.py
@@ -6,7 +6,7 @@ from waddles_cli.helpers.decorators import requires_config
 from waddles_cli.constants.keywords import BACKUP_PATH
 from waddles_cli.helpers.file_manipulation import write_file
 
-from waddles_cli.lib.zimbra.actions.download import download_single_package
+from waddles_cli.lib.zimbra.models.session import ZimbraUserAPI
 
 if TYPE_CHECKING:  # pragma: no cover
     from waddles_cli.models import config
@@ -14,18 +14,30 @@ if TYPE_CHECKING:  # pragma: no cover
 
 
 @requires_config
-def download(params: Namespace, config_output: config.ConfigLoader):
-    if params.option == "single":
-        download_single(params=params, config_output=config_output)
+def download(params: Namespace, config_output: config.ConfigLoader) -> bool:
+    """ An abstraction for the method download_single or download_multiple
 
-
-def download_single(params: Namespace, config_output: config.ConfigLoader):
-    """ Return the active global_config for this user.
-
-    :param params:
+    :param params: A Namespace object with the parameters informed by the user
     :param config_output: All the global_config values available
     """
-    output = download_single_package(username=params.username, zimbra_config=config_output.zimbra)
+    if params.option == "single":
+        download_single(params=params, config_output=config_output)
+    return True
+
+def download_single(params: Namespace, config_output: config.ConfigLoader) -> bool:
+    """ Return the active global_config for this user.
+
+    :param params: A Namespace object with the parameters informed by the user
+    :param config_output: All the global_config values available
+    """
+    zimbra_connection = ZimbraUserAPI(admin_username=config_output.zimbra.admin_username,
+                                      admin_password=config_output.zimbra.admin_password,
+                                      webproto=config_output.zimbra.webproto,
+                                      server_address=config_output.zimbra.server_address,
+                                      admin_port=config_output.zimbra.server_port,
+                                      username=params.username)
+
+    output = zimbra_connection.get_account_tgz()
     path = BACKUP_PATH.format(path=config_output.backup_info.storage_path, username=params.username)
     write_file(request_output=output, backup_path=path)
 
